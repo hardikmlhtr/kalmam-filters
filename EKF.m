eb6d56fbbe4abc348e3be0130baec8a014cc4431
hardikmlhtr <hardikmlhtr@gmail.com>
2015-02-08 02:26:51 +0530

    Update EKF.m

--- EKF.m
+++ EKF.m
@@ -1,23 +1,25 @@
-function [x,P]=ekf(fstate,x,P,hmeas,z,Q,R)
+function [x,P]=ekf(func,x,P,func2,z,Q,R)
+% Inputs:   f: function handle for f(x)
+%           x: previous state estimate
+%           P: previous estimated state covariance
+%           h: function handle for h(x)
+%           z: current measurement
+%           Q: process noise covariance 
+%           R: measurement noise covariance
 
-[x1,A]=jaccsd(fstate,x);    %nonlinear update and linearization at current state
-P=A*P*A'+Q;                 %partial update
-[z1,H]=jaccsd(hmeas,x1);    %nonlinear measurement and linearization
+% Output:   x:  state estimate
+%           P:  state covariance
+[x1,A]=jacob(func,x);       %linearization of first function
+P=A*P*A'+Q;                 
+[z1,H]=jacob(func2,x1);     %linearization of second function
 P12=P*H';                   %cross covariance
-% K=P12*inv(H*P12+R);       %Kalman filter gain
-% x=x1+K*(z-z1);            %state estimate
-% P=P-K*P12';               %state covariance matrix
-R=chol(H*P12+R);            %Cholesky factorization
-U=P12/R;                    %K=U/R'; Faster because of back substitution
-x=x1+U*(R'\(z-z1));         %Back substitution to get state update
-P=P-U*U';                   %Covariance update, U*U'=P12/R/R'*P12'=K*P12.
+K=P12/(H*P12+R);            %Kalman filter gain
+x=x1+K*(z-z1);              %state estimate
+P=P-K*P12';                 %state covariance matrix
 
-function [z,A]=jaccsd(fun,x)
-% JACCSD Jacobian through complex step differentiation
-% [z J] = jaccsd(f,x)
-% z = f(x)
-% J = f'(x)
-%
+
+function [z,A]=jacob(fun,x)
+% Jacobian of function fun
 z=fun(x);
 n=numel(x);
 m=numel(z);
